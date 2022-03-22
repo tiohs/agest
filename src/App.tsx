3c970da5fe106cf14ce36214a3fe59d813e09168
tiohs <hamiltonsilva551@gmail.com>
2022-03-22 16:24:00 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
 import theme from './global/theme';
 import { Dashboard } from './screens/Dashboard';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
--- src/global/theme.ts
+++ src/global/theme.ts
@@ -15,5 +15,10 @@ export default {
     title: '#363F5F',
     text: '#969CB2',
     background: '#F0F2F5'
+  },
+  fonts: {
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
   }
-}
+};
