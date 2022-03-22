3af842707a90206d4c03b53a86818cc849fcd9e2
tiohs <hamiltonsilva551@gmail.com>
2022-03-22 17:39:00 +0100

    ok

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container, Text } from './styles';
+import {
+  Container,
+  Header,
+  UserInfo,
+  User,
+  UserGreeting,
+  UserName,
+  Photo,
+  UserWrapper
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Text>Dashboard</Text>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/53865238?v=4' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Hamilton Silva</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,14 +1,47 @@
+import { RFPercentage, RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
-
 export const Container = styled.View`
   flex: 1;
+  background-color: ${({ theme }) => theme.colors.background};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: ${RFPercentage(42)}px;
+  background-color: ${({ theme }) => theme.colors.primary};
   justify-content: center;
   align-items: center;
-  background-color: ${({ theme }) => theme.colors.primary};
+  flex-direction: row;
 `;
-export const Text = styled.Text`
+
+export const UserWrapper = styled.View`
+  width: 100%;
+  padding: 0 24px;
+`;
+
+export const UserInfo = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const User = styled.View`
+  margin-left: 17px;
+`;
+
+export const UserGreeting = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+`;
+
+export const UserName = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-size: ${RFValue(18)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
-  color: ${({ theme }) => theme.colors.text};
-  font-size: 24px;
-  font-weight: bold;
+`;
+export const Photo = styled.Image`
+  width: ${RFValue(52)}px;
+  height: ${RFValue(52)}px;
+
+  border-radius: 10px;
 `;
