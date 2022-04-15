f612733651abbdefcad8f7823d39b3d8808be27f
tiohs <hamiltonsilva551@gmail.com>
2022-04-15 15:17:06 +0100

    Cards

--- src/components/HighlightCard/styled.ts
+++ src/components/HighlightCard/styled.ts
@@ -10,14 +10,32 @@ export const Container = styled.View`
   padding-bottom: ${RFValue(42)}px;
 `;
 
-export const Header = styled.View``;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(14)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
 
-export const Icon = styled(Feather)``;
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(40)}px;
+`;
 
 export const Footer = styled.View``;
 
-export const Amount = styled.Text``;
+export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
+  margin-top: 38px;
+`;
 
-export const LastTransaction = styled.Text``;
+export const LastTransaction = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(12)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
--- src/global/theme.ts
+++ src/global/theme.ts
@@ -14,6 +14,7 @@ export default {
     shape: '#FFFFFF',
     title: '#363F5F',
     text: '#969CB2',
+    text_dark: '#000',
     background: '#F0F2F5'
   },
   fonts: {
