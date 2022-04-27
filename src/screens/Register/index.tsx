0ce6c666e77ee24988dc67efd073d9df477c9005
tiohs <hamiltonsilva551@gmail.com>
2022-04-27 15:34:14 +0100

    Forms

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Input } from '../../components/Forms/Input';
 import { Container, Header, Title } from './styles';
 
 export function Register() {
@@ -8,6 +9,7 @@ export function Register() {
       <Header>
         <Title>Cadastro</Title>
       </Header>
+      <Input placeholder="Nome" />
     </Container>
   );
 }
