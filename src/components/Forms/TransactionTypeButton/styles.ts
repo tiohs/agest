af43e9918b6303c33bdb01cedc2fcca4ed83a937
tiohs <hamiltonsilva551@gmail.com>
2022-04-28 16:39:13 +0100

    Stelyd Transaction

--- src/components/Forms/TransactionTypeButton/styles.ts
+++ src/components/Forms/TransactionTypeButton/styles.ts
@@ -1,9 +1,26 @@
 import { Feather } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
-export const Container = styled(TouchableOpacity)``;
+export const Container = styled(TouchableOpacity)`
+  width: 100%;
 
-export const Title = styled.Text``;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 
-export const Icon = styled(Feather)``;
+  border: 1.5px solid ${({ theme }) => theme.colors.text};
+  border-radius: 5px;
+  padding: 16px 59px;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(26)}px;
+  margin-right: 12px;
+`;
